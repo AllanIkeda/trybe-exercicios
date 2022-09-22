2b30f9238e4c69d31708e8d34fe31211b18ab4a4
Allan Ikeda <allanikeda@outlook.com>
2022-09-22 10:02:21 -0300

    exercicio 4.2, exercicio 2 indice

--- fundamentos/bloco-04-introducao-javascript-logica-de-programacao/dia-02-array-loop-for/script.js
+++ fundamentos/bloco-04-introducao-javascript-logica-de-programacao/dia-02-array-loop-for/script.js
@@ -1,5 +1,13 @@
-//exercicio1
-let menu = ['Home', 'Serviços', 'Portfólio', 'Links'];
-let menuServices = menu[1];
+// //exercicio1
+// let menu = ['Home', 'Serviços', 'Portfólio', 'Links'];
+// let menuServices = menu[1];
 
-console.log(menuServices);
+// console.log(menuServices);
+
+//exercicio2
+let menu = ['Home', 'Serviços', 'Portfólio', 'Links'];
+let indexOfPortfolio = menu.indexOf('Portfólio');
+
+console.log(indexOfPortfolio);
+
+// exercicio3
