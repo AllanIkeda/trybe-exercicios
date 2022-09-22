6cebf27167afce4898b5950cdc0f155158181c62
Allan Ikeda <allanikeda@outlook.com>
2022-09-22 11:25:06 -0300

    exercicio 4.2, exercicio1 for

--- fundamentos/bloco-04-introducao-javascript-logica-de-programacao/dia-02-array-loop-for/for.js
+++ fundamentos/bloco-04-introducao-javascript-logica-de-programacao/dia-02-array-loop-for/for.js
@@ -1,6 +1,14 @@
-//teste +=
-let numero = 7
+// //teste +=
+// let numero = 7
 
-for (let contador = 1; contador <= 10; contador += 1) {
-  console.log(contador + ' X ' + (numero) + ' = ' + numero * contador);
+// for (let contador = 1; contador <= 10; contador += 1) {
+//   console.log(contador + ' X ' + (numero) + ' = ' + numero * contador);
+// }
+
+// exercicio1
+
+let groceryList = ['Arroz', 'Feijão', 'Alface', 'Melancia'];
+
+for (index = 0; index < groceryList.length; index += 1) {
+  console.log(groceryList[index]);
 }
