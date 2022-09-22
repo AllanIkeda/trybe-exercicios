cca58748539023996cc3f6d9f3141d5e7bb510f0
Allan Ikeda <allanikeda@outlook.com>
2022-09-22 12:03:11 -0300

    exercicio 4.2, exercicio 5

--- fundamentos/bloco-04-introducao-javascript-logica-de-programacao/dia-02-array-loop-for/exercicios.js
+++ fundamentos/bloco-04-introducao-javascript-logica-de-programacao/dia-02-array-loop-for/exercicios.js
@@ -25,18 +25,24 @@ let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 
 // exercicio4
 
-let div = 0
-let soma = 0
+// let div = 0
+// let soma = 0
 
-for (let i = 0; i < numbers.length; i += 1) { 
-  soma += numbers[i]
-  div += 1
-}
-if  ((soma / div) > 20) {
-  console.log('Valor maior que 20')
-}else{
-  console.log("valor menor que 20");
-}
+// for (let i = 0; i < numbers.length; i += 1) { 
+//   soma += numbers[i]
+//   div += 1
+// }
+// if  ((soma / div) > 20) {
+//   console.log('Valor maior que 20')
+// }else{
+//   console.log("valor menor que 20");
+// }
 
 // exercicio5
+let maior = 0;
 
+for (let n of numbers) {
+  if ( n > maior ) {
+    maior = n;
+  }
+}console.log(maior);
