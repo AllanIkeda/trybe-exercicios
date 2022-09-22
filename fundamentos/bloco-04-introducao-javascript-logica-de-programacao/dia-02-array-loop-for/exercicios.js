0faa83ef8eb1f07e4bea5f97d0dfe60840533c8a
Allan Ikeda <allanikeda@outlook.com>
2022-09-22 11:52:58 -0300

    exercicio 4.2, exercicio 1 e 2

---
+++ fundamentos/bloco-04-introducao-javascript-logica-de-programacao/dia-02-array-loop-for/exercicios.js
@@ -0,0 +1,15 @@
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+
+// exercicio1
+
+// for (let i = 0; i < numbers.length; i+= 1) {
+//   console.log(numbers[i]);
+// }
+
+// exercicio2
+let soma = 0
+for (let i = 0; i < numbers.length; i += 1) {
+  soma += numbers[i]
+}
+
+console.log(soma);
---
+++ fundamentos/bloco-04-introducao-javascript-logica-de-programacao/dia-02-array-loop-for/while.js
@@ -0,0 +1,19 @@
+// // exemplo
+
+// let counter = 0;
+
+// while(counter !== 5) {
+//   counter += 1;
+//   console.log(counter);
+// }
+
+
+// function dice () { 
+//   return Math.ceil(Math.random() * 6);
+// }
+// let d1 = dice();
+// let d2 = dice();
+// while (d1 === d2) {
+//   d2 = dice();
+// }
+
