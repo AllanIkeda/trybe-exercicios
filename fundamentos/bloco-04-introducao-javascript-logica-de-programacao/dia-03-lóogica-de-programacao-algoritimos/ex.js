2aed31ee6ecad4293fa738719d4a0558b9529168
Allan Ikeda <allanikeda@outlook.com>
2022-09-23 11:33:29 -0300

    exercicio 4.3, fatorial

--- fundamentos/bloco-04-introducao-javascript-logica-de-programacao/dia-03-lóogica-de-programacao-algoritimos/ex.js
+++ fundamentos/bloco-04-introducao-javascript-logica-de-programacao/dia-03-lóogica-de-programacao-algoritimos/ex.js
@@ -1,10 +1,41 @@
-let fruits = [3, 4, 10, 1, 12];
-let soma = 0
-for ( let i = 0; i < fruits.length; i += 1) {
-  soma += fruits[i]
-}
-if  (soma > 15) {
-  console.log(soma)
-} else {
-  console.log('valor menor que 16')
-}
+
+// for ( let i = 0; i < fruits.length; i += 1) {
+//   soma += fruits[i]
+// }
+// if  (soma > 15) {
+//   console.log(soma)
+// } else {
+// //   console.log('valor menor que 16')
+// // }
+// let fruits = [3, 4, 10, 1, 12];
+// let soma = 0
+// fruits.forEach(f => {
+//   // console.log(f)
+//   soma += f
+// });
+
+// if  (soma > 15) {
+//     console.log(soma)
+//   }else {
+//     console.log('valor menor que 16')
+//   }
+
+// const n = 9;
+
+// let resultado = 100;
+// for (let index = 0; index <= n; index += 1) {
+//   resultado -= index;
+// };
+// console.log(resultado);
+
+// let array = [];
+// soma = 0;
+// for (let i = 0; i <= 100; i += 1) {
+//   array.push(i);
+// }
+// array.forEach(e => {
+//   soma += e;
+  
+// });
+// console.log(soma);
+
---
+++ fundamentos/bloco-04-introducao-javascript-logica-de-programacao/dia-03-lóogica-de-programacao-algoritimos/exercicios.js
@@ -0,0 +1,11 @@
+// exercicio 1
+
+// let fatorial = 10;
+// let resultado = fatorial
+
+// for (let i = 1; i < 10; i += 1) {
+//   resultado *= (fatorial - 1);
+//   fatorial -= 1
+// }
+// console.log(resultado);
+
