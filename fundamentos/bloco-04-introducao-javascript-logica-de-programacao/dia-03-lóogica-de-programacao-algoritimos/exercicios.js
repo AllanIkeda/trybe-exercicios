63d8a26282f7969bc9db6998f5bbe402d55f5ecf
Allan Ikeda <allanikeda@outlook.com>
2022-09-23 12:12:30 -0300

    exercicio 4.3, maior menor

--- fundamentos/bloco-04-introducao-javascript-logica-de-programacao/dia-03-lóogica-de-programacao-algoritimos/exercicios.js
+++ fundamentos/bloco-04-introducao-javascript-logica-de-programacao/dia-03-lóogica-de-programacao-algoritimos/exercicios.js
@@ -12,14 +12,34 @@
 
 // exercicio2
 
-let word = 'tryber';
-let array = word.split('');
-let arrayReversed = [];
-// let arrayReversed = array.reverse();
-for (let index = 0; index < array.length; index += 1) {
-    arrayReversed.unshift (array[index]);
+// let word = 'tryber';
+// let array = word.split('');
+// let arrayReversed = [];
+// // let arrayReversed = array.reverse();
+// for (let index = 0; index < array.length; index += 1) {
+//     arrayReversed.unshift (array[index]);
+// }
+
+// // console.log(arrayReversed);
+// console.log(arrayReversed.join(''));
+// console.log(arrayReversed);
+
+//exercicio3
+
+let array = ['java', 'javascript', 'python', 'html', 'css'];
+let menor = array[0];
+let maior = array[0];
+
+for (let i = 0; i < array.length; i+= 1) {
+  if (array[i].length > maior.length) {
+    maior = array[i];
+  }
+}
+for (let i = 0; i < array.length; i+= 1) {
+  if (array[i].length < menor.length) {
+    menor = array[i];
+  }
 }
 
-// console.log(arrayReversed);
-console.log(arrayReversed.join(''));
-console.log(arrayReversed);
+console.log(maior);
+console.log(menor);
