ed326229ec43ffd0c470906f2bfc39da28fed034
Allan Ikeda <allanikeda@outlook.com>
2022-09-23 15:14:13 -0300

    exercicio 4.3, maior menor

--- fundamentos/bloco-04-introducao-javascript-logica-de-programacao/dia-03-lóogica-de-programacao-algoritimos/exercicios.js
+++ fundamentos/bloco-04-introducao-javascript-logica-de-programacao/dia-03-lóogica-de-programacao-algoritimos/exercicios.js
@@ -26,20 +26,21 @@
 
 //exercicio3
 
-let array = ['java', 'javascript', 'python', 'html', 'css'];
-let menor = array[0];
-let maior = array[0];
+// let array = ['java', 'javascript', 'python', 'html', 'css'];
+// let menor = array[0];
+// let maior = array[0];
 
-for (let i = 0; i < array.length; i+= 1) {
-  if (array[i].length > maior.length) {
-    maior = array[i];
-  }
-}
-for (let i = 0; i < array.length; i+= 1) {
-  if (array[i].length < menor.length) {
-    menor = array[i];
-  }
-}
+// for (let i = 0; i < array.length; i+= 1) {
+//   if (array[i].length > maior.length) {
+//     maior = array[i];
+//   }
+// }
+// for (let i = 0; i < array.length; i+= 1) {
+//   if (array[i].length < menor.length) {
+//     menor = array[i];
+//   }
+// }
 
-console.log(maior);
-console.log(menor);
+// console.log(maior);
+// console.log(menor);
+
