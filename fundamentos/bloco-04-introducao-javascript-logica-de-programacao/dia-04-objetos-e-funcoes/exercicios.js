0dcb309f3e0a2408e6970e2e4248172c27ab3aa9
Allan Ikeda <allanikeda@outlook.com>
2022-09-26 12:32:35 -0300

    exercicios dia 04, ex05

--- fundamentos/bloco-04-introducao-javascript-logica-de-programacao/dia-04-objetos-e-funcoes/exercicios.js
+++ fundamentos/bloco-04-introducao-javascript-logica-de-programacao/dia-04-objetos-e-funcoes/exercicios.js
@@ -10,6 +10,22 @@ info.recorrente = 'sim'
 // console.log('Bem-vinda, '+ info.personagem);
 // console.log(info['recorrente']);
 
-for (let key in info) {
-  console.log(info[key]);
+// for (let key in info) {
+//   console.log(info[key]);
+// }
+
+let info2 = {
+  personagem: 'Tio Patinhas',
+  origem: 'Christmas on Bear Mountain, Dell’s Four Color Comics #178’',
+  nota: 'O último MacPatinhas',
+  recorrente: 'sim'
+}
+
+for (let key in info && info2) {
+  if (info[key] != info2[key]) {
+    console.log(info[key] + ' e ' + info2[key]);
+  }
+  if (info[key] === info2[key]) {
+    console.log('Ambos recorrente')
+  }
 }
