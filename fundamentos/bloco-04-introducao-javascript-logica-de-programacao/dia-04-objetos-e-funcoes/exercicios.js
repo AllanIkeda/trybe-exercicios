e0342ed01512896cd9a71cbe54e4d965861d2205
Allan Ikeda <allanikeda@outlook.com>
2022-09-26 12:46:52 -0300

    exercicios dia 04, ex07

--- fundamentos/bloco-04-introducao-javascript-logica-de-programacao/dia-04-objetos-e-funcoes/exercicios.js
+++ fundamentos/bloco-04-introducao-javascript-logica-de-programacao/dia-04-objetos-e-funcoes/exercicios.js
@@ -43,4 +43,14 @@ let leitor = {
   ],
 };
 
-console.log('O livro favorito de ' + leitor['nome', 'sobrenome'] + ' se chama ' + leitor.livrosFavoritos[0].titulo);
+// console.log('O livro favorito de ' + leitor['nome', 'sobrenome'] + ' se chama ' + leitor.livrosFavoritos[0].titulo);
+
+leitor.livrosFavoritos.push (
+  {
+    titulo: 'Harry Potter e o Prisioneiro de Azkaban',
+    autor: 'JK Rowling',
+    editora: 'Rocco',
+  }
+)
+
+console.log(leitor.nome + ' tem ' + leitor.livrosFavoritos.length + ' livros favoritos');
