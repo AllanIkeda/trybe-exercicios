bd1566657d7180de41adf60cf60e63a16221eee5
Allan Ikeda <allanikeda@outlook.com>
2022-09-28 15:59:04 -0300

    exercicios, terceiro requisito OK

--- fundamentos/secao-05-DOM-eventos-e-web-storage/dia-01-DOM-e-seletores/exJS.js
+++ fundamentos/secao-05-DOM-eventos-e-web-storage/dia-01-DOM-e-seletores/exJS.js
@@ -3,7 +3,7 @@
       - document.getElementById()
       - document.getElementsByClassName()
       - document.getElementsByTagName()
-      3. Crie e execute uma função que mude a cor do quadrado vermelho para branco.
+
       4. Crie e execute uma função que corrija o texto da tag <h1>.
       5. Crie e execute uma função que modifique o texto da primeira tag <p> para maiúsculo.
       6. Crie e execute uma função que exiba o conteúdo de todas as tags <p> no console.
@@ -19,9 +19,16 @@ function bgTrybe (){
   bg.style.backgroundColor = 'rgb(30, 183, 32)';
 }
 
+function bgWhite() {
+  let bga = document.getElementsByClassName('center-content')[0];
+  bga.style.backgroundColor = 'white';
+}
+
 
 //1
 mudarTexto();
 //2
 bgTrybe();
 //3
+bgWhite();
+//4
