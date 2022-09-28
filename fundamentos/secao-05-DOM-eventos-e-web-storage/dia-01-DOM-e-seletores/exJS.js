f0d8e923f50e15f4f3bc61cad4a75cbc535a4390
Allan Ikeda <allanikeda@outlook.com>
2022-09-28 15:42:38 -0300

    exercicios, primeiro requisito OK

---
+++ fundamentos/secao-05-DOM-eventos-e-web-storage/dia-01-DOM-e-seletores/exJS.js
@@ -0,0 +1,18 @@
+   /*
+      Aqui você vai modificar os elementos já existentes utilizando apenas as funções:
+      - document.getElementById()
+      - document.getElementsByClassName()
+      - document.getElementsByTagName()
+      1. Crie e execute uma função que mude o texto na tag `<p>-----</p>`, para uma descrição de como você se vê daqui a 2 anos. (Não gaste tempo pensando no texto e sim realizando o exercício)
+      2. Crie e execute uma função que mude a cor do quadrado amarelo para o verde da Trybe (rgb(76,164,109)).
+      3. Crie e execute uma função que mude a cor do quadrado vermelho para branco.
+      4. Crie e execute uma função que corrija o texto da tag <h1>.
+      5. Crie e execute uma função que modifique o texto da primeira tag <p> para maiúsculo.
+      6. Crie e execute uma função que exiba o conteúdo de todas as tags <p> no console.
+      */
+
+function mudarTexto (txt) {
+  let paragrafo = document.getElementsByTagName('p')[1];
+  paragrafo.innerHTML = 'ganhando um dinheirinho ai';
+}
+mudarTexto();
