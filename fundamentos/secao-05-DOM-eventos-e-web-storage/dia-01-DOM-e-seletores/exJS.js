737c38840f1bd1f268033a4ca6a26b731f45844a
Allan Ikeda <allanikeda@outlook.com>
2022-09-28 15:53:17 -0300

    exercicios, segundo requisito OK

--- fundamentos/secao-05-DOM-eventos-e-web-storage/dia-01-DOM-e-seletores/exJS.js
+++ fundamentos/secao-05-DOM-eventos-e-web-storage/dia-01-DOM-e-seletores/exJS.js
@@ -3,16 +3,25 @@
       - document.getElementById()
       - document.getElementsByClassName()
       - document.getElementsByTagName()
-      1. Crie e execute uma função que mude o texto na tag `<p>-----</p>`, para uma descrição de como você se vê daqui a 2 anos. (Não gaste tempo pensando no texto e sim realizando o exercício)
-      2. Crie e execute uma função que mude a cor do quadrado amarelo para o verde da Trybe (rgb(76,164,109)).
       3. Crie e execute uma função que mude a cor do quadrado vermelho para branco.
       4. Crie e execute uma função que corrija o texto da tag <h1>.
       5. Crie e execute uma função que modifique o texto da primeira tag <p> para maiúsculo.
       6. Crie e execute uma função que exiba o conteúdo de todas as tags <p> no console.
       */
 
-function mudarTexto (txt) {
+function mudarTexto () {
   let paragrafo = document.getElementsByTagName('p')[1];
   paragrafo.innerHTML = 'ganhando um dinheirinho ai';
 }
+
+function bgTrybe (){
+  let bg = document.getElementsByClassName('main-content')[0];
+  bg.style.backgroundColor = 'rgb(30, 183, 32)';
+}
+
+
+//1
 mudarTexto();
+//2
+bgTrybe();
+//3
