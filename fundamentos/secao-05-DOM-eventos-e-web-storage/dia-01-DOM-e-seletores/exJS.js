c4cb423e80c6cbc46c5a1ab39a47336a7ad71278
Allan Ikeda <allanikeda@outlook.com>
2022-09-28 16:10:00 -0300

    exercicios, quarto e quinto requisito OK

--- fundamentos/secao-05-DOM-eventos-e-web-storage/dia-01-DOM-e-seletores/exJS.js
+++ fundamentos/secao-05-DOM-eventos-e-web-storage/dia-01-DOM-e-seletores/exJS.js
@@ -1,13 +1,4 @@
-   /*
-      Aqui você vai modificar os elementos já existentes utilizando apenas as funções:
-      - document.getElementById()
-      - document.getElementsByClassName()
-      - document.getElementsByTagName()
 
-      4. Crie e execute uma função que corrija o texto da tag <h1>.
-      5. Crie e execute uma função que modifique o texto da primeira tag <p> para maiúsculo.
-      6. Crie e execute uma função que exiba o conteúdo de todas as tags <p> no console.
-      */
 
 function mudarTexto () {
   let paragrafo = document.getElementsByTagName('p')[1];
@@ -24,7 +15,18 @@ function bgWhite() {
   bga.style.backgroundColor = 'white';
 }
 
+function h1correct() {
+  let h1 = document.getElementsByTagName('h1')[0];
+  h1.innerHTML = 'Exercício - JavaScript';
+}
 
+function upcase() {
+  let paragrafoUp = document.getElementsByTagName('p')[0];
+  paragrafoUp.innerHTML = paragrafoUp.innerHTML.toUpperCase();
+}
+
+
+// 6. Crie e execute uma função que exiba o conteúdo de todas as tags <p> no console.
 //1
 mudarTexto();
 //2
@@ -32,3 +34,6 @@ bgTrybe();
 //3
 bgWhite();
 //4
+h1correct();
+//5
+upcase();
