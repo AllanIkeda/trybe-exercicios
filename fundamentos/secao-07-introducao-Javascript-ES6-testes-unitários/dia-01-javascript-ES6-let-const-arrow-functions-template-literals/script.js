19f124fd0ea8afd9dcdb3405330dbf5e7452a394
Allan Ikeda <allanikeda@outlook.com>
2022-10-17 15:50:01 -0300

    exercico 1

---
+++ fundamentos/secao-07-introducao-Javascript-ES6-testes-unitários/dia-01-javascript-ES6-let-const-arrow-functions-template-literals/script.js
@@ -0,0 +1,38 @@
+// testando variaveis
+// function testingScope(escopo) {
+//   if (escopo === true) {
+//     const ifScope = 'Não devo ser utilizada fora do meu escopo (if)';
+//     ifScope = ifScope + ' ótimo, fui utilizada no escopo !';
+//     console.log(ifScope);
+//   } else {
+//     let elseScope = 'Não devo ser utilizada fora do meu escopo (else)';
+//     console.log(elseScope);
+//   }
+//   // console.log(ifScope + ' o que estou fazendo aqui ? :O');
+// }
+
+// testingScope(true);
+
+const oddsAndEvens = [13, 3, 4, 10, 7, 2];
+
+const sortArrayBonus = (array) => {
+  const sortOddsAndEvens = array.sort((a, b) => a - b);
+  return sortOddsAndEvens;
+};
+
+const sortedArrayBonus = sortArrayBonus(oddsAndEvens);
+console.log(`Os números ${sortedArrayBonus} se encontram ordenados de forma crescente!`);
+
+
+// const oddsAndEvens = [13, 3, 4, 10, 7, 2];
+
+// console.log(`Os números ${oddsAndEvens.sort((a, b) => a - b)} se encontram ordenados de forma crescente!`);
+
+const factorial = (N) => {
+  let result = 1;
+  for (let i = 2; i <= N; i += 1) {
+    result *= i;
+  }
+  return `o fatorial de ${N} é ${result}`;
+}
+console.log(factorial(10));
