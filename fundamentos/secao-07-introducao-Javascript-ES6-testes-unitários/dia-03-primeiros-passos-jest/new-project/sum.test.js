775d7c60b6fe860671f751835a3d782bc553b196
Allan Ikeda <allanikeda@outlook.com>
2022-10-19 19:00:19 -0300

    exercicio 1 completo

--- fundamentos/secao-07-introducao-Javascript-ES6-testes-unitários/dia-03-primeiros-passos-jest/new-project/sum.test.js
+++ fundamentos/secao-07-introducao-Javascript-ES6-testes-unitários/dia-03-primeiros-passos-jest/new-project/sum.test.js
@@ -2,6 +2,15 @@ const {sum} = require('./sum');
 
 describe('exercicio 1', () => {
   it ('teste se o retorno de sum 4 + 5 é 9', () => {
-    expect(sum(4, 5)).toBe(9)
+    expect(sum(4, 5)).toBe(9);
+  });
+  it ('teste se o retorno de sum 0 + 0 é 0', () => {
+    expect(sum(0, 0)).toBe(0);
+  });
+  it ('teste se o retorno de sum retorna error', () => {
+    expect(() => sum(4, '5')).toThrowError();
+  });
+  it ('teste se o retorna a mensagem parameters must be numbers', () => {
+    expect(() => sum(1, '1')).toThrowError('parameters must be numbers');
   });
 });
