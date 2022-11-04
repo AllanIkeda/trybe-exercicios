f1ed4eb38de2c2dc236aa3fa16afac9553a1e1b6
Allan Ikeda <allanikeda@outlook.com>
2022-11-04 17:04:17 -0300

    password generator

--- front-end/secao-01-introducao-frontend-e-js-assincrono/dia-01-ambiente-de-desenvoolvimento/src/main.js
+++ front-end/secao-01-introducao-frontend-e-js-assincrono/dia-01-ambiente-de-desenvoolvimento/src/main.js
@@ -1,1 +1,10 @@
-console.log('Hello world');
+import { nanoid } from 'nanoid';
+import './style.css';
+
+const passwordBtnEl = document.querySelector('button');
+const displayPasswordEl = document.querySelector('h2');
+
+passwordBtnEl.addEventListener('click', () => {
+  const randomPassword = nanoid();
+  displayPasswordEl.innerHTML = randomPassword;
+});
