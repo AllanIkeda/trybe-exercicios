e711233cf0740a6a7fb99f87a7341e58430cf6e6
Allan Ikeda <allanikeda@outlook.com>
2022-11-08 18:18:31 -0300

    exercicio

---
+++ front-end/secao-01-introducao-frontend-e-js-assincrono/dia-01-ambiente-de-desenvoolvimento/projeto-validador/src/main.js
@@ -0,0 +1,27 @@
+import validator from 'validator';
+import './style.css';
+
+const campoDeTexto = document.querySelector('#value');
+const botao = document.querySelector('#button');
+const seletor = document.querySelector('#option');
+const textoDeSaida = document.querySelector('#answer');
+const UUID_VERSION = 4;
+
+botao.addEventListener('click', (e) => {
+  e.preventDefault();
+  const campos = {
+    cpf: validator.isTaxID(campoDeTexto.value, 'pt-BR'),
+    hexColor: validator.isHexColor(campoDeTexto.value),
+    email: validator.isEmail(campoDeTexto.value),
+    uuid: validator.isUUID(campoDeTexto.value, UUID_VERSION),
+    url: validator.isURL(campoDeTexto.value),
+  };
+  let resp = '';
+  Object.keys(campos).forEach((chave) => {
+    resp = chave;
+    resp.toLocaleUpperCase();
+    seletor.value.toLocaleUpperCase();
+    if (resp === seletor.value) return resp;
+  });
+  textoDeSaida.innerHTML = `A validação retornou ${resp}`;
+});
