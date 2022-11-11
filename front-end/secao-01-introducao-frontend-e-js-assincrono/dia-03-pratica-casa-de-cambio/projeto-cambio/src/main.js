62de842a8319ee10b746a81baa4223e100561934
Allan Ikeda <allanikeda@outlook.com>
2022-11-11 14:52:40 -0300

    att final

--- front-end/secao-01-introducao-frontend-e-js-assincrono/dia-03-pratica-casa-de-cambio/projeto-cambio/src/main.js
+++ front-end/secao-01-introducao-frontend-e-js-assincrono/dia-03-pratica-casa-de-cambio/projeto-cambio/src/main.js
@@ -1,7 +1,67 @@
+import './style.css';
+import Swal from 'sweetalert2';
+const moeda = document.querySelector('#moeda');
+const pesquisar = document.querySelector('#btn');
+const resultDiv = document.querySelector('#result');
+const tituloMoney = document.querySelector('.tituloMoney');
+const resultContainer = document.querySelector('.resultContainer');
+const main = document.querySelector('#main');
 
 
-const titulo = document.querySelector('#titulo');
-const divPesquisar = document.querySelector('#divPesquisa');
-const getInput = document.querySelector('#inputMoeda');
-const getBtn = document.querySelector('#btnPesquisar');
-const getBoard = document.querySelector('#showMoedas')
+const creath2 = (base) => {
+  tituloMoney.innerHTML = `Valores referentes a 1 ${base}`
+}
+const createSpan = (rates) => {
+  const arr = Object.keys(rates);
+  arr.forEach((element) => {
+    const span = document.createElement('span');
+    const coin = document.createElement('div');
+    const amount = document.createElement('div');
+    amount.style.color = '#FFE713';
+    // amount.style.
+    coin.className = 'bi bi-coin';
+    span.appendChild(coin);
+    span.appendChild(amount);
+    span.className = 'resultText';
+    coin.innerHTML = `  ${element}`;
+    amount.innerHTML = ` ${rates[element].toFixed(2)} `
+    resultDiv.appendChild(span);
+  });
+};
+
+const clearSpans = () => {
+  const spans = document.querySelectorAll('.resultText');
+  spans.forEach((span) => {
+    span.remove();
+  });
+};
+
+const errorMessages = (error) => {
+  Swal.fire({
+    icon: 'error',
+    title: error,
+    text: 'Digite uma moeda!'
+  });
+};
+
+pesquisar.addEventListener('click', () => {
+  const moedaValue = moeda.value.toUpperCase();
+  const url = `https://api.exchangerate.host/latest?base=${moedaValue}`;
+  if (moedaValue === '') {
+    errorMessages('Campo vazio!');
+  } else {
+    fetch(url)
+      .then((response) => response.json())
+      .then(({ base, rates }) => {
+        clearSpans();
+        if (base !== moedaValue) {
+          errorMessages('Moeda não encontrada!');
+        } else {
+          resultContainer.style.display = 'block';
+          main.style.justifyContent = 'space-around';
+          createSpan(rates, base);
+          creath2(base);
+        }
+      });
+  }
+});
