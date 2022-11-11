5d849c1bf0cb8e2f82c95d5218bea340a912c0fa
Allan Ikeda <allanikeda@outlook.com>
2022-11-11 14:46:19 -0300

    miniProjeto cambio finalizado

---
+++ front-end/secao-01-introducao-frontend-e-js-assincrono/dia-03-pratica-casa-de-cambio/projeto-cambio/src/main.js
@@ -0,0 +1,7 @@
+
+
+const titulo = document.querySelector('#titulo');
+const divPesquisar = document.querySelector('#divPesquisa');
+const getInput = document.querySelector('#inputMoeda');
+const getBtn = document.querySelector('#btnPesquisar');
+const getBoard = document.querySelector('#showMoedas')
