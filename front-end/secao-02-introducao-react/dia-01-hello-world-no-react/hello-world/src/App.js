1c27a6098eaa83b4d28d7d0aba7ffa90a9aa0b1f
Allan Ikeda <allanikeda@outlook.com>
2022-11-23 15:05:36 -0300

    att

---
+++ front-end/secao-02-introducao-react/dia-01-hello-world-no-react/hello-world/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Text from './about'
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <Text />
+      <h2>Allan Ikeda</h2>
+      <p>Sou pessoa estudante da Trybe</p>
+    </div>
+  );
+}
+
+export default App;
